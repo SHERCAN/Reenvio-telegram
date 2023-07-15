c90d8e0a2efdb579ee74d3ae88c4436214b6db7b
SHERCAN <sidereoyess@gmail.com>
2023-07-15 11:45:11 -0500

    v0.0.1

--- prubea.py
+++ prubea.py
@@ -1,55 +1,51 @@
-from time import sleep
-
-import undetected_chromedriver as uc
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-pathProfile = os.getenv("PATH_PROFILE")
-profile = os.getenv("PROFILE")
-pathChromeDriver = os.getenv("PATH_DRIVER")
+import re
 from selenium.webdriver.chrome.options import Options
 
-if __name__ == "__main__":
-    # chrome_options = uc.ChromeOptions()
-    # chrome_options.add_argument(f"user-data-dir={pathProfile}")
-    # chrome_options.add_argument(f"--profile-directory={profile}")
-    # # chrome_options.add_argument(
-    # #     "--window-size=375,812"
-    # # )  # Tamaño de la ventana del dispositivo móvil
-    # chrome_options.add_argument(
-    #     r"--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1"
-    # )  # Agrega el user-agent de un dispositivo móvil
-    # driver = uc.Chrome(
-    #     options=chrome_options,
-    #     use_subprocess=True,
-    # )
-    # driver.get("https://m.youtube.com/")
-    # sleep(5)
 
-    options = Options()
-    mobile_emulation = {
-        "deviceMetrics": {"width": 360, "height": 640, "pixelRatio": 3.0},
-        "userAgent": "Mozilla/5.0 (Linux; Android 10; SM-G973F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Mobile Safari/537.36",
-    }
-    # options.add_experimental_option("prefs", mobile_emulation)
-    # "prefs", {"credentials_enable_service": False, "profile.password_manager_enabled": False})
-    options.add_argument(f"user-data-dir={pathProfile}")
-    options.add_argument(f"--profile-directory={profile}")
-    options.add_argument(
-        "--user-agent=Mozilla/5.0 (Linux; Android 11; SM-G9910) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.181 Mobile Safari/537.36"
-    )
-    # options.add_argument(f"--user-agent={user_agent}")
-    options.add_argument("--disable-blink-features=AutomationControlled")
-    # mobile_emulation = {"deviceName": "iPhone X"}
-    # options.add_experimental_option("mobileEmulation", mobile_emulation)
+class Variables:
+    def __init__(self):
+        self.api_id = 3979768
+        self.api_hash = "d7cb423a678000f98d3fcc9388041b6f"
+        self.phone = "+573012041605"
+        self.session_file = "yefardi"
+        self.password = ""
+        self.grupoChat = -548326689
+        self.comercialChat = 5534289586
+        self.tare = 0
 
-    driver = uc.Chrome(options=options)
-    driver.set_window_size(600, 800)
-    driver.get("https://m.youtube.com/watch?v=dAh7fUFGHa0")
-    sleep(10)
-    foto = driver.get_screenshot_as_png()
-    with open("foto.png", "wb") as file:
-        file.write(foto)
-    driver.get("https://www.google.com")
-    sleep(10)
+    def __call__(self):
+        return self.__dict__
+
+
+setattr(Variables, "tare", 1)
+Variables.tare = 2
+print(Variables.tare)
+
+
+class Telegram(Options):
+    def __init__(self) -> None:
+        self.options = Options
+        self.options.add_argument(f"user-data-dir={Variables().pathProfile}")
+        self.options.add_argument(f"--profile-directory={Variables().profile}")
+
+
+class Expresiones:
+    def __init__(self) -> None:
+
+        # Expresión regular para buscar la palabra "prepago"
+        self.prepago_regex = re.compile(r"\bprepago\b", re.IGNORECASE)
+        self.comercial_regex = re.compile(r"\bcomercial\b", re.IGNORECASE)
+
+        # Expresión regular para buscar enlaces de YouTube
+        self.youtube_regex = re.compile(r"\.?youtube\.com\/\S+")
+        self.youtube2_regex = re.compile(r"/youtu\.be/")
+
+    def __call__(self):
+        return self.__dict__
+
+
+Expresiones
+text = "De http://youtube.com/ caterine"
+url_regex = re.compile(r"https?://\S+")
+urls_encontradas = re.findall(url_regex, text)
+print(urls_encontradas)
--- telegram.py
+++ telegram.py
@@ -2,7 +2,6 @@ from PIL import Image
 import undetected_chromedriver as uc
 from dotenv import load_dotenv
 from time import sleep, asctime
-from requests import post
 from telethon import TelegramClient, events
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -11,120 +10,130 @@ import re
 from selenium.webdriver.chrome.options import Options
 import os
 
-load_dotenv()
-pathProfile = os.getenv("PATH_PROFILE")
-profile = os.getenv("PROFILE")
-pathChromeDriver = os.getenv("PATH_DRIVER")
-api_id = 3979768
-api_hash = "d7cb423a678000f98d3fcc9388041b6f"
-phone = "+573012041605"
-session_file = "yefardi"  # use your username if unsure
-password = ""  # if you have two-step verification enabled
+
+class Variables:
+    def __init__(self):
+        load_dotenv()
+        self.pathProfile = os.getenv("PATH_PROFILE")
+        self.profile = os.getenv("PROFILE")
+        self.pathChromeDriver = os.getenv("PATH_DRIVER")
+        self.api_id = 3979768
+        self.api_hash = "d7cb423a678000f98d3fcc9388041b6f"
+        self.phone = "+573012041605"
+        self.session_file = "yefardi"
+        self.password = ""
+        self.grupoChat = -548326689
+        self.comercialChat = 5534289586
+        self.tarea = 0
+
+    def __call__(self):
+        return self.__dict__
 
 
-def envio(mess):
-    post(
-        "https://api.telegram.org/bot1672385199:AAGw8Wocay7hrLUJIqNYZmcpZiLPtk-fla8/sendMessage",
-        data={"chat_id": "5534289586", "text": str(mess)},
-    )
+class Expresiones:
+    def __init__(self) -> None:
+
+        # Expresión regular para buscar la palabra "prepago"
+        self.prepago_regex = re.compile(r"\bprepago\b", re.IGNORECASE)
+        self.comercial_regex = re.compile(r"\bcomercial\b", re.IGNORECASE)
+
+        # Expresión regular para buscar enlaces de YouTube
+        self.youtube_regex = re.compile(r"\.?youtube\.com\/\S+")
+        self.youtube2_regex = re.compile(r"/youtu\.be/")
+
+    def __call__(self):
+        return self.__dict__
 
 
-def deci(mon, red, precio):
-    print(mon, red, precio)
-    return round(float(mon) / (1 / float(precio)), red)
+class Telegram():
+    def __init__(self) -> None:
+        self.options = Options()
+        self.options.add_argument(f"user-data-dir={Variables().pathProfile}")
+        self.options.add_argument(f"--profile-directory={Variables().profile}")
+        self.options.add_argument(
+            "--user-agent=Mozilla/5.0 (Linux; Android 11; SM-G9910) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.181 Mobile Safari/537.36"
+        )
+        self.options.add_argument(
+            "--disable-blink-features=AutomationControlled")
+        self.driver = uc.Chrome(options=self.options)
+        self.driver.set_window_size(600, 800)
+        sleep(1)
+        self.driver.get("https://m.youtube.com/")
+        sleep(1)
+        self.client = TelegramClient(
+            Variables().session_file, Variables().api_id, Variables().api_hash, sequential_updates=True)
 
 
-# Expresión regular para buscar la palabra "prepago"
-prepago_regex = re.compile(r"\bprepago\b", re.IGNORECASE)
-comercial_regex = re.compile(r"\bcomercial\b", re.IGNORECASE)
+tele = Telegram()
 
-# Expresión regular para buscar enlaces de YouTube
-youtube_regex = re.compile(r"\.?youtube\.com\/\S+")
-youtube2_regex = re.compile(r"/youtu\.be/")
 
+@tele.client.on(events.NewMessage(chats=Variables().grupoChat))
+async def handle_new_message(event):
+    message = event.message
+    chat_id = message.chat_id
+    text = message.message
+    if Expresiones().prepago_regex.search(text) and Expresiones().comercial_regex.search(text):
+        print(
+            f"Mensaje con la palabra 'prepago' recibido en el grupo {chat_id}: {text}"
+        )
+        Variables().tarea += 1
+    if Expresiones().youtube_regex.search(text) or Expresiones().youtube2_regex.search(text):
+        print(
+            f"Mensaje con enlace de YouTube recibido en el grupo {chat_id}: {text}"
+        )
+        Variables().tarea += 1
+        url_regex = re.compile(r"https?://\S+")
+        urls_encontradas = re.findall(url_regex, text)
+        for url in urls_encontradas:
+            tele.driver.get(url)
+            print(url)
+        sleep(10)
+        texto = (
+            WebDriverWait(tele.driver, 10)
+            .until(
+                EC.visibility_of_element_located(
+                    (
+                        By.XPATH,
+                        '//*[@id="app"]/div[1]/ytm-watch/div[2]/ytm-single-column-watch-next-results-renderer/ytm-slim-video-metadata-section-renderer/ytm-slim-owner-renderer/div/ytm-subscribe-button-renderer/yt-smartimation/div/div/div/button/div/span',
+                    )
+                )
+            )
+            .text
+        )
+        if texto != "Suscrito":
+            meGusta = WebDriverWait(tele.driver, 10).until(
+                EC.visibility_of_element_located(
+                    (
+                        By.XPATH,
+                        '//*[@id="app"]/div[1]/ytm-watch/div[2]/ytm-single-column-watch-next-results-renderer/ytm-slim-video-metadata-section-renderer/ytm-slim-video-action-bar-renderer/div/yt-smartimation/div/ytm-segmented-like-dislike-button-renderer/div/toggle-button-with-animated-icon',
+                    )
+                )
+            )
+            meGusta.click()
+            suscribir = WebDriverWait(tele.driver, 10).until(
+                EC.element_to_be_clickable(
+                    (
+                        By.XPATH,
+                        '//*[@id="app"]/div[1]/ytm-watch/div[2]/ytm-single-column-watch-next-results-renderer/ytm-slim-video-metadata-section-renderer/ytm-slim-owner-renderer/div',
+                    )
+                )
+            )
+            suscribir.click()
+
+        sleep(5)
+        tele.driver.save_screenshot("cap.png")
+        img = Image.open("cap.png")
+        cropped = img.crop((0, 0, 566, 712))  # (left, upper, right, lower)
+        cropped.save("captura.png")
+        group_entity = await tele.client.get_entity(Variables().grupoChat)
+        await tele.client.send_file(
+            group_entity, "captura.png", caption=f"Tarea {Variables().tarea}"
+        )
+        chat_entity = await tele.client.get_entity(Variables().comercialChat)
+        await tele.client.send_file(chat_entity, "captura.png", caption=f"Tarea {Variables().tarea}")
 if __name__ == "__main__":
-    tarea = 0
-    options = Options()
-    options.add_argument(f"user-data-dir={pathProfile}")
-    options.add_argument(f"--profile-directory={profile}")
-    options.add_argument(
-        "--user-agent=Mozilla/5.0 (Linux; Android 11; SM-G9910) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.181 Mobile Safari/537.36"
-    )
-    options.add_argument("--disable-blink-features=AutomationControlled")
-    driver = uc.Chrome(options=options)
-    driver.set_window_size(600, 800)
-    sleep(2)
-    driver.get("https://m.youtube.com/")
-    client = TelegramClient(session_file, api_id, api_hash, sequential_updates=True)
-    group_id = -548326689
-
-    @client.on(events.NewMessage(chats=group_id))
-    async def handle_new_message(event):
-        global tarea
-        print(tarea)
-        message = event.message
-        chat_id = message.chat_id
-        text = message.message
-        if prepago_regex.search(text) and comercial_regex.search(text):
-            print(
-                f"Mensaje con la palabra 'prepago' recibido en el grupo {chat_id}: {text}"
-            )
-            tarea += 1
-        if youtube_regex.search(text) or youtube2_regex.search(text):
-            print(
-                f"Mensaje con enlace de YouTube recibido en el grupo {chat_id}: {text}"
-            )
-            tarea += 1
-            url_regex = re.compile(r"https?://\S+")
-            urls_encontradas = re.findall(url_regex, text)
-            for url in urls_encontradas:
-                driver.get(url)
-            sleep(10)
-            texto = (
-                WebDriverWait(driver, 10)
-                .until(
-                    EC.visibility_of_element_located(
-                        (
-                            By.XPATH,
-                            '//*[@id="app"]/div[1]/ytm-watch/div[2]/ytm-single-column-watch-next-results-renderer/ytm-slim-video-metadata-section-renderer/ytm-slim-owner-renderer/div/ytm-subscribe-button-renderer/yt-smartimation/div/div/div/button/div/span',
-                        )
-                    )
-                )
-                .text
-            )
-            if texto != "Suscrito":
-                meGusta = WebDriverWait(driver, 10).until(
-                    EC.visibility_of_element_located(
-                        (
-                            By.XPATH,
-                            '//*[@id="app"]/div[1]/ytm-watch/div[2]/ytm-single-column-watch-next-results-renderer/ytm-slim-video-metadata-section-renderer/ytm-slim-video-action-bar-renderer/div/yt-smartimation/div/ytm-segmented-like-dislike-button-renderer/div/toggle-button-with-animated-icon',
-                        )
-                    )
-                )
-                meGusta.click()
-                suscribir = WebDriverWait(driver, 10).until(
-                    EC.element_to_be_clickable(
-                        (
-                            By.XPATH,
-                            '//*[@id="app"]/div[1]/ytm-watch/div[2]/ytm-single-column-watch-next-results-renderer/ytm-slim-video-metadata-section-renderer/ytm-slim-owner-renderer/div',
-                        )
-                    )
-                )
-                suscribir.click()
-
-            sleep(5)
-            driver.save_screenshot("cap.png")
-            img = Image.open("cap.png")
-            cropped = img.crop((0, 0, 566, 712))  # (left, upper, right, lower)
-            cropped.save("captura.png")
-            group_entity = await client.get_entity(group_id)
-            await client.send_file(
-                group_entity, "captura.png", caption=f"Tarea {tarea}"
-            )
-            chat_entity = await client.get_entity(5534289586)
-            await client.send_file(chat_entity, "captura.png", caption=f"Tarea {tarea}")
 
     print(asctime(), "-", "Auto-replying...oi")
-    client.start(phone, password)
-    client.run_until_disconnected()
+    tele.client.start(Variables().phone, Variables().password)
+    tele.client.run_until_disconnected()
     print(asctime(), "-", "Stopped!")
